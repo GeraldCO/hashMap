8978f071fec5e1b7740614cea507dd1f9edd775e
Gerald <geraldcerd@gmail.com>
2025-12-22 23:48:32 -0600

    hashmap complete

--- hashmap.js
+++ hashmap.js
@@ -4,27 +4,40 @@ class Hashmap{
         //Each bucket will store an aray of [key, value] pairs
         this.capacity = initialCapacity;
         this.loadFactor = loadFactor;
-        this.buckets = new Array(16);
+        this.buckets = new Array(initialCapacity);
         this.size = 0;
         
     }
 
-    hash(key){
-        let hashCode = 0;
 
-        const primeNumber = 31;
-        for(let i = 0; i < key.length; i++){
-            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+
+       hash(key) {
+        // Base hash value
+        let hash = 0;
+
+        // Prime number for better key distribution
+        const PRIME = 31;
+
+        // Build the hash from the string characters
+        for (let char of key) {
+            hash = hash * PRIME + char.charCodeAt(0);
+
+            // Force hash into 32-bit integer range
+            hash |= 0;
         }
 
-        return hashCode;
+        // Map hash to a valid bucket index
+        return Math.abs(hash) % this.buckets.length;
     }
 
     set(key, value){
-        var index = this.hash(key);
+        var index = this.hash(key);        
         if(this.buckets[index]){
             this.buckets[index] = new Array({key, value});
         }else{
+            if( this.size >= (this.capacity * 0.75)){
+                this.resize();
+            }
             this.buckets[index] = new Array();
             this.buckets[index].push({key,  value})
             this.size = this.size + 1;
@@ -35,7 +48,7 @@ class Hashmap{
     get(key){
 
         if(this.buckets[this.hash(key)]){
-            return  this.buckets[this.hash(key)][0][key]
+            return this.buckets[this.hash(key)][0].value
         }else {
             return null
         }
@@ -65,6 +78,33 @@ class Hashmap{
 
     clear(){
         this.buckets = new Array(16);
+        this.size = 0;
     }
 
+    keys(){
+        var keys = new Array();
+        this.buckets.map((e) => {
+            keys.push(e.key)
+        });
+        return keys;
+    }
+
+    values(){
+        var values = new Array();
+        this.buckets.map((e) => {
+            values.push(e.value);
+        })
+        return values;
+    }
+
+    resize(){
+        this.capacity = this.capacity * 2;
+        var newA = new Array(32);
+        let i = 0;
+        this.buckets.map((e)=>{
+            newA[i] = e;
+            i++;
+        });
+        this.buckets = newA;
+    }
 }
